93f24a06e2483cac027d7d2fbefb6d2d8eccff9b
Angela <angelalamluu@gmail.com>
2021-04-22 09:34:21 -0700

    created functioning login action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      #display errors
+      render :new
+    end
   end
 
   def destroy

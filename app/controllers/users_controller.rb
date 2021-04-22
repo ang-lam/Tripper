cc15217f452c9ae64834cfea1c38bbff5647c3b0
Angela <angelalamluu@gmail.com>
2021-04-22 09:30:28 -0700

    created login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,10 @@ class UsersController < ApplicationController
     end
 
     def create
+        binding.pry
         user = User.new(user_params)
         if user.save
-            session[:user_id] = user.user #log user in
+            session[:user_id] = user.id #log user in
             redirect_to root_path
         else
             #show errors
@@ -15,4 +16,11 @@ class UsersController < ApplicationController
             render :new
         end
     end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
+
 end

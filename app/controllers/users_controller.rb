db60bc4c8f52aa3929861e4dbcc0ec135323faad
Angela <angelalamluu@gmail.com>
2021-04-22 09:12:17 -0700

    created signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,14 @@ class UsersController < ApplicationController
     end
 
     def create
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.user #log user in
+            redirect_to root_path
+        else
+            #show errors
+            #render signup form again
+            render :new
+        end
     end
 end

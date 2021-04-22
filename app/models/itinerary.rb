eb57cf7cf2bde241b186b8ef233fba1e99838f3a
Angela <angelalamluu@gmail.com>
2021-04-21 22:39:38 -0700

    added password to users table

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,2 +1,5 @@
 class Itinerary < ApplicationRecord
+
+    belongs_to :user
+    belongs_to :destination
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :itineraries
+  has_many :destinations, through: :itineraries
+
 end

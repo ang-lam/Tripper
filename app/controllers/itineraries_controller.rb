f7c52067de099c9e1ae90a7473f194d3f83ae56d
Angela <angelalamluu@gmail.com>
2021-04-25 22:56:38 -0700

    created new itinerary with nested activities form

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -4,8 +4,18 @@ class ItinerariesController < ApplicationController
     end
    
     def new
+        @itinerary = Itinerary.new
+        8.times {@itinerary.activities.build}
+
     end
 
     def create
     end
+
+    private
+
+    def itinerary_params
+        params.require(:itinerary).permit(:date, activities_attributes: [:time, :description])
+    end
+
 end
--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -2,4 +2,6 @@ class Itinerary < ApplicationRecord
     belongs_to :user
     belongs_to :destination
     has_many :activities, dependent: :destroy
+
+    accepts_nested_attributes_for :activities
 end

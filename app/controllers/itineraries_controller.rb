634bc6952623ab76dbbc1de1a6007dd0b7e37cc4
Angela <angelalamluu@gmail.com>
2021-04-26 22:56:14 -0700

    refactored itinerary new and create actions

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -8,29 +8,46 @@ class ItinerariesController < ApplicationController
     def new
         @destination = Destination.find_by(id: params[:destination_id])
         @itinerary = @destination.itineraries.build
-        8.times {@itinerary.activities.build}
+        @itinerary.activities.build
+        # 8.times {@itinerary.activities.build}
     end
 
     def create
-        @user = session[:user_id]
         @destination = Destination.find_by(id: params[:destination_id])
         @itinerary = @destination.itineraries.build(itinerary_params)
         @itinerary.user_id = session[:user_id]
-        
         if @itinerary.save
-            itinerary_params[:activities_attributes].each do |activity|
-                if activity[1][:time] != "" && activity[1][:description] != ""
-                    @activity = @itinerary.activities.build(activity[1])
-                    if @activity.save
-                        next
-                    end
-                end
-            end
+            redirect_to destination_itinerary_path(@destination, @itinerary)
         else
             render :new
         end
-        redirect_to destination_itinerary_path(@destination, @itinerary)
+
+        # @destination = Destination.find_by(id: params[:destination_id])
         # @itinerary = @destination.itineraries.build(itinerary_params)
+        # @itinerary.user_id = session[:user_id]
+        # if @itinerary.save
+        #     itinerary_params[:activities_attributes].each do |activity|
+        #         binding.pry
+        #         if activity[1][:time] != "" && activity[1][:description] != ""
+        #             @activity = @itinerary.activities.build(activity[1])
+        #             if @activity.save
+        #                 next
+        #             end
+        #         end
+        #     end
+        # else
+        #     render :new
+        # end
+        # redirect_to destination_itinerary_path(@destination, @itinerary)
+        # @itinerary = @destination.itineraries.build(itinerary_params)
+        #try to have two buttons:
+        # 1. submits and renders the itinerary page
+        # 2. submits and brings you back to new page
+        
+        # or 
+
+        # on destination page create an itinerary with x number of activities
+
     end
 
     private

d0d53cd5d1ff6d97a9f358b05d9508dd5232f806
Angela <angelalamluu@gmail.com>
2021-04-26 22:59:42 -0700

    added signup validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   has_many :itineraries
   has_many :destinations, through: :itineraries
 
+  validates :first_name, :last_name, presence: true
+  validates :password, length: { in: 6..10 }
+  validate :is_email?
+
+  def is_email?
+    if !email.match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i)
+      errors.add(:email, "not a valid email")
+    end 
+  end 
 end

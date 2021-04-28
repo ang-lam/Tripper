6f45d71592ce96ee54660b77e142c229edcb4014
Angela <angelalamluu@gmail.com>
2021-04-27 22:46:14 -0700

    added destroy action in itineraries controller

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -3,7 +3,7 @@ class ItinerariesController < ApplicationController
     def index
     end
 
-    
+
     def show
         @itinerary = Itinerary.find_by(id: params[:id])
         @activities = @itinerary.activities
@@ -25,33 +25,13 @@ class ItinerariesController < ApplicationController
         else
             render :new
         end
+    end
 
-        # @destination = Destination.find_by(id: params[:destination_id])
-        # @itinerary = @destination.itineraries.build(itinerary_params)
-        # @itinerary.user_id = session[:user_id]
-        # if @itinerary.save
-        #     itinerary_params[:activities_attributes].each do |activity|
-        #         binding.pry
-        #         if activity[1][:time] != "" && activity[1][:description] != ""
-        #             @activity = @itinerary.activities.build(activity[1])
-        #             if @activity.save
-        #                 next
-        #             end
-        #         end
-        #     end
-        # else
-        #     render :new
-        # end
-        # redirect_to destination_itinerary_path(@destination, @itinerary)
-        # @itinerary = @destination.itineraries.build(itinerary_params)
-        #try to have two buttons:
-        # 1. submits and renders the itinerary page
-        # 2. submits and brings you back to new page
+    def destroy
+        @itinerary = Itinerary.find_by(id: params[:id])
+        @itinerary.destroy
         
-        # or 
-
-        # on destination page create an itinerary with x number of activities
-
+        redirect_to destination_itineraries_path(@itinerary.destination)
     end
 
     private

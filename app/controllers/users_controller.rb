4acf4097013affa72b024589b3132431563892f6
Angela <angelalamluu@gmail.com>
2021-04-28 22:28:04 -0700

    fixed create action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
     end
 
     def create
-        user = User.new(user_params)
-        if user.save
+        @user = User.new(user_params)
+        if @user.save
             session[:user_id] = user.id #log user in
             redirect_to root_path
         else

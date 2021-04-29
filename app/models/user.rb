2fedbfaeb2bb5b681d61dc3c0ee20fe8954ad6f2
Angela <angelalamluu@gmail.com>
2021-04-28 22:02:51 -0700

    changed password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   has_many :itineraries
   has_many :destinations, through: :itineraries
 
-  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
-  validates :password, length: { in: 6..10 }
+  validates :password, length: { in: 6..20 }
   validate :is_email?
 
   def is_email?

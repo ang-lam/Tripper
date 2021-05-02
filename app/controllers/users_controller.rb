4f34030e8281167b822184051f8f448bbe81dc14
Angela <angelalamluu@gmail.com>
2021-05-01 23:46:20 -0700

    fixed syntax error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            session[:user_id] = user.id #log user in
+            session[:user_id] = @user.id #log user in
             redirect_to root_path
         else
             #show errors

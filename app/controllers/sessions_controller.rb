6ed6fdce3d6e2dfd69f223a6acdffe3d0912f1c3
Angela <angelalamluu@gmail.com>
2021-05-01 20:56:11 -0700

    added comment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path
     else
-      #display errors
+      #display errors with flash? cannot use #errors
       render :new
     end
   end

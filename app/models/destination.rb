1f686516d4ccaee43e0d9f7eda595dd5b4e80c64
Angela <angelalamluu@gmail.com>
2021-05-07 23:23:09 -0700

    fixed collection_select_destination_name syntax

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -6,7 +6,6 @@ class Destination < ApplicationRecord
 
     validates :city, :country, presence: true
     validate :destination_entered_is_unique
-    #add custom validation method to make sure city and state are both uniq so that no duplicate
 
     def destination_entered_is_unique
         if Destination.find_by(city: city, state: state)
@@ -15,9 +14,8 @@ class Destination < ApplicationRecord
     end
 
     def collection_select_destination_name
-        "#{city.split.map(&:capitalize).join(' ')}, #{state.split.map(&:capitalize).join(' ') if state}, #{country.split.map(&:capitalize).join(' ')}"
+        "#{city.split.map(&:capitalize).join(' ')}, #{state.split.map(&:capitalize).join(' ') if state}#{"," if state.present?} #{country.split.map(&:capitalize).join(' ')}"
     end
 
     scope :popular_trips, -> {joins(:itineraries).group('id').order('count(destinations.id) desc').limit(2)}
-    # scope :users_trips, -> (user) {joins(:itineraries).where(id = "destination_id").order(:date).uniq}
 end

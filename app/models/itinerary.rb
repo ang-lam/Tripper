e1ea4f78c0266fbbda009c9103708c1096a0a120
Angela <angelalamluu@gmail.com>
2021-05-09 02:01:02 -0700

    removed past date validation

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -4,18 +4,18 @@ class Itinerary < ApplicationRecord
     has_many :activities, dependent: :destroy
 
     accepts_nested_attributes_for :activities,
-        allow_destroy: true, 
-        reject_if: proc {|attr| attr['time'].blank? && attr['description'].blank?}
+        allow_destroy: true,
+        reject_if: proc {|act| act['time'].blank? && act['description'].blank?}
 
-    validate :date_cannot_be_in_the_past
 
-    def date_cannot_be_in_the_past
-        if !date.present? || date < Date.today
-            errors.add(:date, "cannot be in the past or empty")
-        end
-    end
+    # validate :date_cannot_be_in_the_past
+    validates :date, presence: true
+    # def date_cannot_be_in_the_past
+    #     if Itinerary.exists?(date: date, user_id: user_id, destination_id: destination_id) && date < Date.today
+    #         errors.add(:date, "cannot be in the past")
+    #     end
+    # end
 
     scope :trips_itineraries, -> (user, destination) {where(destination_id: destination, user_id: user.id)}
     scope :order_itineraries, -> {order(:date)}
-
 end

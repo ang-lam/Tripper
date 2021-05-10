7c8dfbc4112dd5d8ae5a6112fb75f763616883cb
Angela <angelalamluu@gmail.com>
2021-05-09 17:01:44 -0700

    removed comments

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -8,13 +8,8 @@ class Itinerary < ApplicationRecord
         reject_if: proc {|act| act['time'].blank? && act['description'].blank?}
 
 
-    # validate :date_cannot_be_in_the_past
+
     validates :date, presence: true
-    # def date_cannot_be_in_the_past
-    #     if Itinerary.exists?(date: date, user_id: user_id, destination_id: destination_id) && date < Date.today
-    #         errors.add(:date, "cannot be in the past")
-    #     end
-    # end
 
     scope :trips_itineraries, -> (user, destination) {where(destination_id: destination, user_id: user.id)}
     scope :order_itineraries, -> {order(:date)}
